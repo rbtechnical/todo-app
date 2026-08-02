eee1ccab3edcf3c0b951fb4ede25542b3b035703
Rohan <rohanbalekundri90@gmail.com>
2026-08-02 12:09:22 +0100

    inital todo app

---
+++ app/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://mongo:27017/tododb');
+
+const Todo = mongoose.model('Todo', { text: String, done: Boolean });
+
+app.get('/todos', async (req, res) => res.json(await Todo.find()));
+app.post('/todos', async (req, res) => {
+  const todo = await Todo.create({ text: req.body.text, done: false });
+  res.json(todo);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
